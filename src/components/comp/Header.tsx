1d1a78253f1e8101f514d96641d75df8e9d58d10
Rômulo Henri <romulohenri000@gmail.com>
2024-03-05 22:22:34 -0300

    add: HeaderLink

--- src/components/comp/Header.tsx
+++ src/components/comp/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from "next/link";
+import HeaderLink from './HeaderLink';
 import { Button } from "@/components/ui/button";
 
 const Header = () => {
@@ -8,28 +8,23 @@ const Header = () => {
         <header className="flex h-20 w-full shrink-0 items-center justify-center
          px-4 md:px-6">
           <div className="flex gap-2">
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/"
-            >
+
+            <HeaderLink href="/">
               Início
-            </Link>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/timeline"
-            >
+            </HeaderLink>
+
+            <HeaderLink href="/timeline">
               Linha do Tempo
-            </Link>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/about"
-            >
+            </HeaderLink>
+
+            <HeaderLink href="/about">
               Sobre
-            </Link>
-          
+            </HeaderLink>
+
             <Button className="justify-self-end px-2 py-1 text-xs" variant="outline">
               Avalie!
-            </Button>          </div>
+            </Button>          
+          </div>
         </header>
       </div>
     )
