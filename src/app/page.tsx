fade945ed7717b6dfabecd809a22bfa311a5fb1d
Rômulo Henri <romulohenri000@gmail.com>
2024-03-05 21:25:35 -0300

    add: Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
+import Header from '@/components/comp/Header';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h2>
-          Next
-        </h2>
-        
+    <main className="min-h-screen flex-col p-24">
+        <Header />
+
         <Button variant="outline" size="sm">
           Button
         </Button>
-      </div>
     </main>
   );
 }
