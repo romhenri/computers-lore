100cf9449cc9f4f818c832c937184dbac6942ed3
Rômulo Henri <romulohenri000@gmail.com>
2024-03-06 17:09:26 -0300

    add: Darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
 import Header from '@/components/comp/Header';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,8 +21,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from '@/components/ui/button';
 
+import ToggleTheme from '@/components/comp/ToggleTheme';
+
 export default function Home() {
   return (
     <main className="">
-        <Button variant="outline" size="sm">
+        {/* <Button variant="outline" size="sm">
           Button
-        </Button>
+        </Button> */}
+
+        <ToggleTheme />
     </main>
   );
 };
