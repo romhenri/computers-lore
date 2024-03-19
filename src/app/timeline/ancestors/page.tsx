81be3100d3e5d617baaf9082a3de32187d12e53a
Rômulo Henri <romulohenri000@gmail.com>
2024-03-19 18:49:14 -0300

    improve: Ancestors Timeline

--- src/app/timeline/ancestors/page.tsx
+++ src/app/timeline/ancestors/page.tsx
@@ -2,12 +2,18 @@ import LineTitle from "@/components/base/line-title";
 import Timeline from "@/components/struct/Timeline";
 import { Button } from "@/components/base/button";
 import { Line } from "@/components/base/line";
-// Link Next
 import Link from 'next/link';
+// Improt font form google
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: '600',
+  preload: false,
+});
 
 const AncestorsPage = () => {
   return (
-    <main className="pt-4 lg:border-x lg:border-b lg:p-4">
+    <main className={`pt-4 lg:border-x lg:border-b lg:p-4 ${montserrat.className}`}>
       <LineTitle>
         1º Ancestrais
       </LineTitle>
@@ -18,22 +24,26 @@ const AncestorsPage = () => {
         tokens="h-[2205px] text-black text-2xl leading-10"
       >
         <div className="pt-[50px]">
-          O abaco surgiu com a ideia de facilitar as contas pra muita gente e fazer calculos das mais diversas formas, introduzinho a ideia de automatizaçõa de tarefas simples e sendo considera o primeiro computador
+          Há milênios, antes mesmo da escrita, o ábaco surgiu como uma ferramenta rudimentar para cálculos.
+          <br/><br/>
+          Com suas contas deslizantes, ele representou o primeiro passo na evolução dos dispositivos de computação, facilitando a matemática e o comércio em civilizações antigas.
         </div>
 
-        <div className="pt-[370px]">
-          O abaco surgiu com a ideia de facilitar as contas pra muita gente e fazer calculos das mais diversas formas, introduzinho a ideia de automatizaçõa de tarefas simples e sendo considera o primeiro computador
+        <div className="pt-[186px]">
+          Criada pelo matemático e filósofo Blaise Pascal, a Pascaline realizava operações aritméticas básicas, como adição e subtração, através de engrenagens e rodas dentadas.
+          <br/><br/>
+          Início da automação dos cálculos, contribuindo para o desenvolvimento posterior das calculadoras e computadores modernos.
         </div>
 
-        <div className="pt-[610px] text-right">
+        <div className="pt-[450px] text-right">
         O abaco surgiu com a ideia de facilitar as contas pra muita gente e fazer calculos das mais diversas formas, introduzinho a ideia de automatizaçõa de tarefas simples e sendo considera o primeiro computador.
         </div>
 
         <div className="pt-[374px]">
           <Link href="/timeline">
             <Button 
-              className="w-full" 
-              variant="secondary" 
+              className="w-full"
+              variant="secondary"
             >
               Concluir Capítulo
             </Button>
--- src/components/struct/Timeline.tsx
+++ src/components/struct/Timeline.tsx
@@ -14,7 +14,15 @@ const Timeline : React.FC<ITimelineProps> = ({
   return (
     <div className="w-full flex justify-center">
       <div className={
-        `w-[500px] ${tokens} ${bg} bg-cover bg-center bg-no-repeat rounded p-4 mb-8`
+        `w-[500px] min-w-[500px] ${tokens} ${bg} bg-cover bg-center bg-no-repeat rounded p-4 mb-8
+        
+        origin-top
+        scale-75 sm:scale-100
+
+        max-[400px]:scale-[0.7]
+        max-[350px]:scale-[0.6]
+        max-[300px]:scale-[0.5]
+        `
       }>
         {
           children
