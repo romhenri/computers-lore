1a340d68b53993480099a9272dd4767f0b92931d
Rômulo Henri <romulohenri000@gmail.com>
2024-03-19 21:29:17 -0300

    add: ChapCard

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,5 +1,6 @@
 import LineTitle from "@/components/base/line-title";
 import { Line } from "@/components/base/line";
+import ChapCard from "@/components/struct/ChapCard";
 
 const Timeline = () => {
   return (
@@ -9,8 +10,18 @@ const Timeline = () => {
       </LineTitle>
 
       <Line />
+
+      <div className="flex justify-center">
+        <ChapCard
+          name="1º - Ancestrais"
+          info="5.500 AC"
+          desc="Primeiros máquinas de cálculo"
+          img="https://img.freepik.com/fotos-premium/abaco-de-madeira-velho-e-antigo-para-contar-e-calcular_1831-703.jpg"
+          pathPage="timeline/ancestors"
+        />
+      </div>
     </main>
-  )
+  );
 };
 
 export default Timeline;
