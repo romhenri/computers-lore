70ef8474b426c6e5c7fdef506e793a4cfcf324a3
Ernandes Tavares <nando.tavares714@gmail.com>
2024-03-25 16:30:30 -0300

    improve: Gen1 Timeline

--- src/app/timeline/gen1/page.tsx
+++ src/app/timeline/gen1/page.tsx
@@ -3,11 +3,45 @@ import Timeline from "@/components/struct/Timeline";
 import { Button } from "@/components/base/button";
 import { Line } from "@/components/base/line";
 import Link from 'next/link';
-// Improt font form google
-import { Montserrat } from "next/font/google";
+import { Montserrat, Bebas_Neue, Koulen, League_Gothic, Krona_One, Kufam, Gugi, Khand } from "next/font/google";
 
 const montserrat = Montserrat({
   weight: '500',
+  preload: false,
+});
+
+const bebasneue = Bebas_Neue({
+  weight: '400',
+  preload: false,
+});
+
+const khand = Khand({
+  weight: '400',
+  preload: false,
+});
+
+const gugi = Gugi({
+  weight: '400',
+  preload: false,
+});
+
+const kufam = Kufam({
+  weight: '400',
+  preload: false,
+});
+
+const krona = Krona_One({
+  weight: '400',
+  preload: false,
+});
+
+const koulen = Koulen({
+  weight: '400',
+  preload: false,
+});
+
+const league = League_Gothic({
+  weight: '400',
   preload: false,
 });
 
@@ -25,11 +59,13 @@ const AncestorsPage = () => {
         bg="bg-timeline3"
         tokens="h-[2680px] text-white text-2xl leading-10"
       >
-        <div className="pt-[58px] pl-[48px]">
-          <h3>
-            ENIAC (1945)
-          </h3>
-          <p>
+        <div className="pt-[48px] pl-[58px]">
+        <h3 className={` text-[2.5rem] ${koulen.className} text-neutral-200`}>
+            <span className=" border-b-4 border-green-600 inline-block">
+             Eniac (1945)
+            </span>
+           </h3>
+          <p className={` pt-[30px] text-neutral-200 `}>
           Foi o primeiro computador eletrônico de grande escala. Utilizando válvulas eletrônicas, revolucionou ao realizar cálculos de forma mais rápida e eficiente.
           <br></br>
           <br></br>
@@ -37,35 +73,38 @@ const AncestorsPage = () => {
           </p>
         </div>
 
-        <div className="pt-[145px] pl-[48px]">
-          <h3 className="">
-            A partir daqui  a computação não foi mais a mesma...
+        <div className="pt-[115px]">
+          <h3 className={`${krona.className} text-center text-green-600`}>
+            A partir daqui a computação não foi mais a mesma...
           </h3>
-          <p>
+          <p className={`${kufam.className} pl-[28px] pl-[58px]`}>
             Inovações como:
           </p>
-          <ol>
-            <li> Programação Manual</li>
-            <li> Armazenando Eletronicamente</li>
-            <li> Cálculos Avançados</li>
+          <ol className={`${kufam.className} pl-[38px]`}>
+            <li> -Programação<span className={`text-lime-200 `}> Manual</span></li>
+            <li> -Armazenando<span className={`text-lime-200`}> Eletronicamente</span></li>
+            <li> -Cálculos<span className={`text-lime-200`}> Avançados</span></li>
           </ol>
         </div>
 
-        <div className="pt-[230px] pl-[48px]">
-          <h3 className="">
-            Colossus I (1943)
+        <div className="pt-[220px]">
+          <h3 className={` pl-[48px] text-[2.5rem] ${koulen.className}`}>
+            Colossus I<span className={`text-green-600 `}> (1943)</span>
           </h3>
-          <p>
+          <p className={` pl-[28px] pt-[25px] text-[1.8rem] ${khand.className} text-green-100`}>
           Entrou em operação em e desempenhou um papel crucial na decifração dos códigos de comunicação alemães. Sua arquitetura inovadora, utilizando válvulas eletrônicas.
-          Além de influenciar o campo da criptoanálise e da tecnologia da informação.
+          Além de influenciar o campo da criptoanálise e da tecnologia da informação.<br/> 
+          <h2 className={`${khand.className} text-green-400`}>Não podemos esperar que uma máquina nos dê uma resposta,<br/>  sem antes fornecermos<br/>  a ela uma pergunta<br/>  adequada. - Alan Turing</h2>
           </p>
         </div>
 
-        <div className="pt-[260px] pl-[28px]">
-          <h3 className="">
-            Colossus II (1944)
+        <div className="pt-[150px] pl-[28px]">
+          <h3 className={` text-[2.4rem] ${koulen.className} text-green-600 text-center`}>
+            <span className=" border-b-4 border-green-100 inline-block ">
+             Colossus II (1944)
+            </span>
           </h3>
-          <p>
+          <p className={` pt-[40px] text-neutral-200`}>
             Implementação de uma arquitetura paralela, permitindo que várias tarefas fossem realizadas simultaneamente. Além disso, capacidade de processamento aprimorada, com velocidade e eficiência aprimoradas. O design do Colossus Mark II também foi simplificado em comparação com o Colossus original, o que ajudou a melhorar sua confiabilidade e facilidade de manutenção.
           </p>
         </div>
