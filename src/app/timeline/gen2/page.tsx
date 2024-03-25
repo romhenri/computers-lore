9de07039bd6d2e15977bd25d031f128efca767f3
Ernandes Tavares <nando.tavares714@gmail.com>
2024-03-25 00:54:18 -0300

    Adiçãodosdetalhes
    detalhes das paginas da geração dois e da geração 3

--- src/app/timeline/gen2/page.tsx
+++ src/app/timeline/gen2/page.tsx
@@ -3,10 +3,25 @@ import Timeline from "@/components/struct/Timeline";
 import { Button } from "@/components/base/button";
 import { Line } from "@/components/base/line";
 import Link from 'next/link';
-import { Montserrat } from "next/font/google";
+import { Montserrat, Bebas_Neue, Koulen, League_Gothic } from "next/font/google";
 
 const montserrat = Montserrat({
   weight: '500',
+  preload: false,
+});
+
+const bebasneue = Bebas_Neue({
+  weight: '400',
+  preload: false,
+});
+
+const koulen = Koulen({
+  weight: '400',
+  preload: false,
+});
+
+const league = League_Gothic({
+  weight: '400',
   preload: false,
 });
 
@@ -24,32 +39,42 @@ const AncestorsPage = () => {
         bg="bg-timeline4"
         tokens="h-[2680px] text-white text-2xl leading-10"
       >
-        <div className="pt-[58px]">
-          <h3>
-            IBM (1964)
-          </h3>
-          <p>
+        <div className="pt-[58px] ">
+          <h2>
+           <h3 className={` text-[3rem] ${koulen.className} text-center`}>
+            <span className="ml-10 border-b-4 border-sky-500 inline-block">
+            IBM/360 (1964)
+            </span>
+           </h3>
+          </h2>
+          <p className={` pt-[50px] text-right text-sky-100`}>
           Com sua arquitetura modular e compatibilidade entre modelos, proporcionou flexibilidade e proteção de investimento aos usuários. Introduziu conceitos como comprimento de palavra variável e memória virtual, influenciando profundamente o 
-          desenvolvimento                    futuro   de computadores. 
-          Vendedor ao lado de um modelo IBM.
+          desenvolvimento<br/>                    futuro   de computadores.<br/> 
+          Vendedor ao lado de<br/> um modelo IBM.<br/>
           </p>
         </div>
 
-        <div className="pt-[480px]">
-          <h3>
-          IBM 7030 (1961)
-          </h3>
-          <p>
-          Introduzindo a arquitetura de palavra de 64 bits e oferecendo um desempenho significativamente superior aos seus contemporâneos. Embora tenha enfrentado desafios durante o desenvolvimento e tenha tido um número limitado de unidades vendidas devido a atrasos e altos custos, o IBM 7030 estabeleceu importantes precedentes tecnológicos e contribuiu para o avanço da computação de alta performance.
+        <div className="pt-[380px]">
+          <div className={`ml-5 text-[1.5rem] ${league.className} `}>
+            Conheça
+            <h3 className={`ml-6 text-[2.5rem] ${koulen.className} text-sky-400`}>
+            IBM 7030 (1961)
+            </h3 >
+          </div>
+          <p className={`ml-5 pt-[30px] text-sky-100`}>
+          Introduzindo a arquitetura de palavra de 64 bits e oferecendo um desempenho superior aos seus contemporâneos. Embora tenha enfrentado desafios durante o desenvolvimento e tenha tido um número limitado de unidades vendidas devido a atrasos e altos custos, o IBM 7030 estabeleceu importantes precedentes tecnológicos e contribuiu para o avanço da computação de alta performance.
           </p>
         </div>
 
         <div className="pt-[200px]">
-          <h3 className="pl-[38px]">
-            IBM 7030 (1961)
+          <h3 className={`pl-[38px] text-[2.5rem] ${koulen.className} text-neutral-200 `}>
+            PDP-8<span className={`text-sky-400`}> (1965)</span >
           </h3>
-          <p>
-          Introduzindo a arquitetura de palavra de 64 bits e oferecendo um desempenho significativamente superior aos seus contemporâneos. Embora tenha enfrentado desafios durante o desenvolvimento e tenha tido um número limitado de unidades vendidas devido a atrasos e altos custos, o IBM 7030 estabeleceu importantes precedentes tecnológicos e contribuiu para o avanço da computação de alta performance.
+          <p className={`ml-5 pt-[30px] text-blue-100`}>
+          Foi notável por seu tamanho compacto, preço acessível e simplicidade de arquitetura. Sua introdução democratizou o acesso à computação, tornando-a viável para empresas, universidades e laboratórios de pesquisa. Apesar de sua simplicidade em comparação com os mainframes da época, sua influência duradoura e
+          contribuição para o desenvolvimento
+          da computação pessoal
+          são inegáveis.
           </p>
         </div>
 
--- src/app/timeline/gen3/page.tsx
+++ src/app/timeline/gen3/page.tsx
@@ -3,10 +3,15 @@ import Timeline from "@/components/struct/Timeline";
 import { Button } from "@/components/base/button";
 import { Line } from "@/components/base/line";
 import Link from 'next/link';
-import { Montserrat } from "next/font/google";
+import { Montserrat, Gugi } from "next/font/google";
 
 const montserrat = Montserrat({
   weight: '500',
+  preload: false,
+});
+
+const gugi = Gugi({
+  weight: '400',
   preload: false,
 });
 
@@ -24,30 +29,36 @@ const AncestorsPage = () => {
         bg="bg-timeline5"
         tokens="h-[2680px] text-white text-2xl leading-10"
       >
-        <div className="pt-[58px]">
-          <h3>
-            Altair 8800 (1975)
-          </h3>
-          <p>
-            Apesar de sua simplicidade, tornou-se extremamente popular entre os entusiastas de computação, impulsionando o surgimento da indústria de computadores pessoais. Sua importância histórica é inegável, já que inspirou uma geração de aficionados por tecnologia e ajudou a pavimentar o caminho para a era dos computadores pessoais. Já tinha um microprocessador.
+        <div className=" pt-[58px]">
+        <h3 className={`text-[2rem] ${gugi.className} text-2xl pb-[10px] text-yellow-200`}>
+         <span className="border-b-4 border-violet-400">
+         Altair 8800 (1975)
+         </span>
+        </h3>
+          <p className={'pt-[48px] text-violet-100'}>
+            Apesar de sua simplicidade, tornou-se extremamente popular entre os entusiastas de computação, impulsionando o surgimento <br/>da indústria de computadores pessoais. Sua importância histórica<br/> é inegável, já que inspirou uma geração de aficionados por <br/>tecnologia e ajudou a pavimentar o caminho para era dos computadores pessoais. A partir disso, os computadores ja continham <br/>um microprocessador.
           </p>
         </div>
 
-        <div className="pt-[398px]">
-          <h3>
-            APPLE I (1976)
+        <div className=" pt-[300px]">
+          <h3 className={`text-[2rem]  ${gugi.className} text-2xl pb-[10px] text-violet-400 text-left`}>
+          <span className="ml-5 border-b-4 border-yellow-200">
+           APPLE I(1976)
+           </span>
           </h3>
-          <p>
+          <p className={' pt-[25px] text-violet-100 text-left'}>
             Sua arquitetura baseada no processador MOS 6502 e sua capacidade de expansão de memória proporcionaram uma base sólida para futuros desenvolvimentos na linha de produtos da Apple. Sua simplicidade e acessibilidade também contribuíram para popularizar o conceito de computador pessoal entre entusiastas e consumidores, preparando o terreno para o sucesso futuro da Apple na indústria de tecnologia.
           </p>
         </div>
 
-        <div className="pt-[368px]">
-          <h3>
-            APPLE II (1977)
-          </h3>
-          <p>
-            Computadores pessoais completos e prontos para uso, diferenciando-se no mercado por sua arquitetura avançada, interface de usuário amigável e recursos inovadores de gráficos e som. Seu sucesso comercial e impacto duradouro ajudaram a estabelecer a Apple como uma líder na indústria de tecnologia. Foi um dos primeiros computadores pessoais a oferecer recursos avançados de gráficos coloridos e som. Isso o tornou ideal para jogos, educação e aplicações multimídia.
+        <div className="pt-[320px]">
+        <h3 className={`text-[2rem] ${gugi.className} text-2xl pb-[10px] text-violet-400`}>
+         <span className="ml-5 border-b-4 border-yellow-200">
+         APPLE II(1977)
+         </span>
+        </h3>
+          <p className={'ml-5 pt-[25px] text-indigo-100'}>
+            Computadores pessoais completos e prontos para uso, diferenciando-se no mercado por sua arquitetura avançada, interface de usuário amigável e recursos inovadores de gráficos e som. Seu sucesso comercial e impacto duradouro ajudaram a estabelecer a Apple como uma líder na indústria de tecnologia. Foi um dos primeiros computadores pessoais a oferecer recursos avançados de gráficos coloridos e som. <br/>Isso o tornou ideal para <br/>jogos, educação e <br/>aplicações multimídia.
           </p>
         </div>
 
